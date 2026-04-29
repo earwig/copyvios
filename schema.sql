f330db4dd4ef98f45c842f00c47662fc83126717
Ben Kurtovic <ben@benkurtovic.com>
2026-04-29 01:03:01 -0400

    Update SQL schema

--- schema.sql
+++ schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE cache (
-    cache_id BLOB NOT NULL,
-    cache_time INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now')),
-    cache_queries INTEGER NOT NULL DEFAULT 0,
-    cache_process_time REAL NOT NULL DEFAULT 0,
-    cache_possible_miss INTEGER NOT NULL DEFAULT 0,
+    cache_id BINARY(32) NOT NULL,
+    cache_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    cache_queries INT(4) NOT NULL DEFAULT 0,
+    cache_process_time DOUBLE NOT NULL DEFAULT 0,
+    cache_possible_miss BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY (cache_id)
 );
 CREATE INDEX cache_time_idx ON cache (cache_time);
 CREATE TABLE cache_data (
-    cdata_id ROWID,
-    cdata_cache_id BLOB NOT NULL,
-    cdata_url TEXT NOT NULL,
-    cdata_confidence REAL NOT NULL DEFAULT 0,
-    cdata_skipped INTEGER NOT NULL DEFAULT 0,
-    cdata_excluded INTEGER NOT NULL DEFAULT 0,
+    cdata_id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    cdata_cache_id BINARY(32) NOT NULL,
+    cdata_url VARCHAR(1024) NOT NULL,
+    cdata_confidence DOUBLE NOT NULL DEFAULT 0,
+    cdata_skipped BOOLEAN NOT NULL DEFAULT 0,
+    cdata_excluded BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY (cdata_id),
     FOREIGN KEY (cdata_cache_id) REFERENCES cache (cache_id)
         ON DELETE CASCADE ON UPDATE CASCADE
